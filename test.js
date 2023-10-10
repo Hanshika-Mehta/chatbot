5cf0072a2b7a846990fdd2dcad1f0e857cf46a2d
Hanshika <hanshika.mehta15@gmail.com>
2023-10-10 18:37:10 +0530

    chatbot

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
 // const { OpenAIEmbeddings } = require( "langchain/embeddings/openai");
 const { MemoryVectorStore } = require("langchain/vectorstores/memory");
-const { RetrievalQAChain, ConversationalRetrievalQAChain } = require("langchain/chains");
+const {
+  RetrievalQAChain,
+  ConversationalRetrievalQAChain,
+} = require("langchain/chains");
 const { ChatOpenAI } = require("langchain/chat_models/openai");
 const { PDFLoader } = require("langchain/document_loaders/fs/pdf");
 // const  ChromaClient  = require( "chromadb");
@@ -9,11 +12,11 @@ const { OpenAIEmbeddingFunction } = require("chromadb");
 const { ChromaClient } = require("chromadb");
 // const { Chroma} = require( "langchain/vectorstores/chroma");
 
-// chroma_client = chromadb.Client()
+// chroma_client = chromadb.Client()cls
 // collection = chroma_client.create_collection("bedCourtney")
 
 const main = async () => {
-  const pdfPath = "./courtney-bed-br.pdf";
+  const pdfPath = "E:/tdc-GenAI/chatbot/documents/courtney-bed-br.pdf";
   const pdfLoader = new PDFLoader(pdfPath);
   const data = await pdfLoader.load();
 
@@ -24,7 +27,6 @@ const main = async () => {
       chunkSize: 500,
       chunkOverlap: 0,
     });
-
     const splitDocs = await textSplitter.splitDocuments(data);
 
     // const embeddings = new OpenAIEmbeddings({
@@ -34,27 +36,23 @@ const main = async () => {
     // console.log(embeddings);
 
     const embedder = new OpenAIEmbeddingFunction({
-      openai_api_key: "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",
+      openai_api_key: "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",
     });
     const client = new ChromaClient({});
     const collection = await client.getOrCreateCollection({
       name: "bedCourtney",
       embeddingFunction: embedder,
     });
-
-    const collection2 = await client.getOrCreateCollection({
-      name: "bed",
-      embeddingFunction: embedder,
-      
-    });
-    console.log('my collection',await client.getCollection({
-      name: 'bedCourtney'
-    }))
-    const vectorStore = await collection.add({
-      ids: ["1"],
-      documents: [splitDocs[0].pageContent],
-    });
-    console.log(vectorStore);
+    for (let i = 0; i < splitDocs.length; i++) {
+      const splitDoc = splitDocs[i];
+      const vectorStore = await collection.add({
+        ids: [(i+30).toString()],
+        documents: [splitDoc.pageContent],
+      });
+    }
+    const database = await collection.get();
+    // console.log(database);
+    // console.log(vectorStore);
     // const vectorStore = await MemoryVectorStore.fromDocuments(splitDocs, embeddings);
     // const vectorStore = await Chroma.fromDocuments(splitDocs, embeddings, chromaArgs);
     // console.log("Vector Dimensionssssss:", vectors[0].length);
@@ -65,12 +63,14 @@ const main = async () => {
 
     const model = new ChatOpenAI({
       modelName: "gpt-3.5-turbo",
-      openAIApiKey: "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",
+      openAIApiKey: "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",
     });
     // const chain = ConversationalRetrievalQAChain.fromLLM(model, collection.asRetriever());
     const response = await collection.query({
-      nResults: 2,
-      queryTexts: ["what are the features of product?"],
+      nResults: 1,
+      queryTexts: [
+        "features of the BlackDecker WDBD10:",
+      ],
     });
     console.log(response);
     performance.now();
